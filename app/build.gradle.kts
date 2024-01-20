b9d4dbd3879e179b61ad62be7e8bbab1d4fc8a1e
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-01-20 23:46:51 +0300

    Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.klepetchat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.klepetchat"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
