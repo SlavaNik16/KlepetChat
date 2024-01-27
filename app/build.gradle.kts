f90d39f8d6be720cc57e2ee4192ebee3620610c9
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-01-27 22:52:18 +0300

    Установил нужные библиотеки в gradle. Добавил некоторые xml файлы. Сделал примерную авторизацию.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,15 @@ android {
 
 dependencies {
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.7.2")
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
