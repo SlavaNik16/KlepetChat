4e3524b8a0ad1f0bfbc5538b54524b27e82036d7
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-03-02 14:47:56 +0300

    Буду делать профиль

---
+++ app/src/main/java/KlepetChat/Activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package KlepetChat.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.klepetchat.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
